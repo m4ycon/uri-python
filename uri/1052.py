d5f4b203cbd47377b93d96a79788a6e390fb3716
Maycon <m.vinnicyus@gmail.com>
2020-09-25 11:36:31 -0300

    1052 uri

---
+++ uri/1052.py
@@ -0,0 +1,8 @@
+month = int(input())
+
+months = ['January', 'February', 'March',
+  'April', 'May', 'June',
+  'July', 'August', 'September',
+  'October', 'November', 'December']
+
+print(months[month - 1])
