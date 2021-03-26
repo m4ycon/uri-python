c76446343786a3ddee4bcce58b931fdd7f8bf890
Maycon Fabio <m.vinnicyus@gmail.com>
2021-03-26 15:52:46 -0300

    padronizando a indentação do deque.py

--- deque.py
+++ deque.py
@@ -1,27 +1,27 @@
 class Deque:
-    def __init__(self):
-        self.__items = []
+  def __init__(self):
+    self.__items = []
 
-    def is_empty(self):
-        return self.__items == []
+  def is_empty(self):
+    return self.__items == []
 
-    def add_front(self, item):
-        self.__items.append(item)
+  def add_front(self, item):
+    self.__items.append(item)
 
-    def add_rear(self, item):
-        self.__items.insert(0, item)
+  def add_rear(self, item):
+    self.__items.insert(0, item)
 
-    def remove_rear(self):
-        return self.__items.pop(0)
+  def remove_rear(self):
+    return self.__items.pop(0)
 
-    def remove_front(self):
-        return self.__items.pop()
+  def remove_front(self):
+    return self.__items.pop()
 
-    def size(self):
-        return len(self.__items)
+  def size(self):
+    return len(self.__items)
 
-    def __str__(self):
-        sdeque = ''
-        for i in self.__items:
-            sdeque += i
-        return sdeque
+  def __str__(self):
+    sdeque = ''
+    for i in self.__items:
+      sdeque += i
+    return sdeque
